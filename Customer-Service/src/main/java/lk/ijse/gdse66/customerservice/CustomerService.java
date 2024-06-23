4fa2f61d7c9c16e65de6325cc27d53c901546f1a
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-23 19:22:50 +0530

    Project Name Refactored

--- Customer-Service/src/main/java/lk/ijse/gdse66/customerservice/CustomerService.java
+++ Customer-Service/src/main/java/lk/ijse/gdse66/customerservice/CustomerService.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerServiceApplication {
+public class CustomerService {
 
     public static void main(String[] args) {
-        SpringApplication.run(CustomerServiceApplication.class, args);
+        SpringApplication.run(CustomerService.class, args);
     }
-
 }
