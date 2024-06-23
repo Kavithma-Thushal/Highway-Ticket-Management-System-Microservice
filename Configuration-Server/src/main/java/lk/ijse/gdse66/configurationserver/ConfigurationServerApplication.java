4db5bb353609dcab6018e54fa66442e71afeebb7
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-23 18:41:12 +0530

    Enable Eureka Discovery Client

--- Configuration-Server/src/main/java/lk/ijse/gdse66/configurationserver/ConfigurationServerApplication.java
+++ Configuration-Server/src/main/java/lk/ijse/gdse66/configurationserver/ConfigurationServerApplication.java
@@ -2,8 +2,12 @@ package lk.ijse.gdse66.configurationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
 public class ConfigurationServerApplication {
 
     public static void main(String[] args) {
