f3b239c81700ef19b7a969889f9640941e7fa5ec
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-23 13:53:34 +0530

    API-Gateway Updated

--- API-Gateway/src/main/java/lk/ijse/gdse66/apigateway/ApiGatewayApplication.java
+++ API-Gateway/src/main/java/lk/ijse/gdse66/apigateway/ApiGatewayApplication.java
@@ -8,6 +8,7 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+        System.out.println("Hello World!");
     }
 
 }
