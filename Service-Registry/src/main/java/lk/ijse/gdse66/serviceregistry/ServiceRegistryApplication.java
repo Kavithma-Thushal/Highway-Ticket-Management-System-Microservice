e91847bc312fef2085139514a9028ef9e46f2b35
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-23 13:47:48 +0530

    ServiceRegistryApplication.java Updated

--- Service-Registry/src/main/java/lk/ijse/gdse66/serviceregistry/ServiceRegistryApplication.java
+++ Service-Registry/src/main/java/lk/ijse/gdse66/serviceregistry/ServiceRegistryApplication.java
@@ -8,6 +8,7 @@ public class ServiceRegistryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceRegistryApplication.class, args);
+        System.out.println("Hello World!");
     }
 
 }
