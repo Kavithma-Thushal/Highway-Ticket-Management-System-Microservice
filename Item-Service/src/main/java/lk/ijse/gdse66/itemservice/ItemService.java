c203027fbb61e0bec62d20112ce9462ad22927ca
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-23 22:52:23 +0530

    Item-Service Added

--- Item-Service/src/main/java/lk/ijse/gdse66/itemservice/ItemService.java
+++ Item-Service/src/main/java/lk/ijse/gdse66/itemservice/ItemService.java
@@ -2,11 +2,22 @@ package lk.ijse.gdse66.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ItemService {
 
     public static void main(String[] args) {
         SpringApplication.run(ItemService.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 }
