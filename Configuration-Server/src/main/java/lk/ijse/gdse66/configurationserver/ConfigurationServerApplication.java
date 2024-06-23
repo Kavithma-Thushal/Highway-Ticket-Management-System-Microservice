77ac3f5aab4f9ec54256bf0ee3e03f24fb59dba7
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-23 13:51:23 +0530

    ConfigurationServerApplication.java Updated

--- Configuration-Server/src/main/java/lk/ijse/gdse66/configurationserver/ConfigurationServerApplication.java
+++ Configuration-Server/src/main/java/lk/ijse/gdse66/configurationserver/ConfigurationServerApplication.java
@@ -8,6 +8,7 @@ public class ConfigurationServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigurationServerApplication.class, args);
+        System.out.println("Hello World!");
     }
 
 }
