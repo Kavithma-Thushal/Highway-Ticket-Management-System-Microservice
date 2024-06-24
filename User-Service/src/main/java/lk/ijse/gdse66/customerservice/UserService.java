262e218ca637b9c0b40d666cf809ec5c560ded3f
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-24 11:31:21 +0530

    User Service Added

---
+++ User-Service/src/main/java/lk/ijse/gdse66/customerservice/UserService.java
@@ -0,0 +1,29 @@
+package lk.ijse.gdse66.customerservice;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserService.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
